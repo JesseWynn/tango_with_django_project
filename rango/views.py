c052def8f2113b3cdbee35f7ebbd395a92a0e1d8
Jesse Wynn <2658646t@student.gla.ac.uk>
2023-02-10 11:53:31 +0000

    chap 4

--- rango/views.py
+++ rango/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Rango says hey there partner!")
 
+def index(request):
+    context_dict = {'boldmessage':'Crunch,creamy,cookie,candy,cupcake!'}
+    return render(request,'rango/index.html',context=context_dict)
